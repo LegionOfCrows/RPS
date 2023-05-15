cc55d71af40a6c2861a10726b5211dcfcd36b3d2
LegionOfCrows <reptileeeeee@gmail.com>
2023-05-16 02:25:26 +0300

    add function to play one round

--- javascript.js
+++ javascript.js
@@ -9,4 +9,30 @@ function getComputerChoice(){
             return "scissors"
         }
     }
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection){
+    let player = playerSelection.toLowerCase()
+    if (player === computerSelection) {
+        console.log("TIE")
+    } else if (player === "rock"){
+        if (computerSelection === "paper"){
+            console.log("YOU LOSE")
+        } else {
+            console.log("YOU WIN")
+        }
+    } else if (player === "paper"){
+        if (computerSelection === "scissors"){
+            console.log("YOU LOSE")
+        } else {
+            console.log("YOU WIN")
+        }   
+    } else if (player === "scissors"){
+        if (computerSelection === "rock"){
+            console.log("YOU LOSE")
+        } else {
+            console.log("YOU WIN")
+        }   
+    }}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
